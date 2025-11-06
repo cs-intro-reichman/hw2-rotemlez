77a31aefd65c2be3069d887bd14d386a4285b32c
rotemlez <rotem.lezerovich@post.runi.ac.il>
2025-11-06 18:52:24 +0200

    completed assignment

--- TimeCalc.java
+++ TimeCalc.java
@@ -3,25 +3,20 @@ public class TimeCalc {
        int hours = Integer.parseInt(args[0].substring(0, 2)); 
 	    int minutes = Integer.parseInt(args[0].substring(3, 5)); 
 		int minutesToAdd = Integer.parseInt(args[1]);
-        System.out.print("houres= " + hours + " minutes= " + minutes + " to add= " + minutesToAdd);
         int totalMinutes= ((hours*60) + minutes + minutesToAdd);
         int totalHours = totalMinutes / 60;
         int newHours = totalHours % 24;
         int newMinutes = (totalMinutes - (totalHours*60));
-        System.out.print(" new houres= " + newHours + " new minutes= " + newMinutes);
         if (newHours<10 && newMinutes<10){
-            System.out.print("   0" + newHours + ":0" + newMinutes);
+            System.out.println("   0" + newHours + ":0" + newMinutes);
         } 
-        else{
-            if (newHours<10){
-            System.out.print("   0" + newHours + ":" + newMinutes);
-             }
-        }
-        if (newMinutes<10){
-            System.out.print("   " + newHours + ":0" + newMinutes);
-        }
-        else{
-           System.out.print("   " + newHours + ":" + newMinutes); 
-        }
+           
+            else{
+                     System.out.println("   " + newHours + ":" + newMinutes); 
+            }
+            
+        
+            
+                
     }
 }
